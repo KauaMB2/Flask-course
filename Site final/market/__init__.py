ebdc76445fda02e9846181a8cb8456850da408fd
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-06-26 19:28:56 -0300

    Hosting...

--- Site final/market/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask#Import flask
-import os
-app = Flask(__name__)#Cria aplicação
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'#Encontra o banco de dados(NECESSÁRIO)
-app.config['SECRET_KEY']='829de852066b68880f990c50'#Uma camada a mais de segurança em relação ao formulário
-imgsFolder=os.path.join('static','img')
-app.config['UPLOAD_FOLDER']=imgsFolder
-
-from market import routes#Importa as rotas(NECESSÁRIO)
